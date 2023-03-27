17517faab9405a3a0f42f76dc3375d45eb6c8569
Szymon Burak <sajmon0031@gmail.com>
2023-03-27 16:42:02 +0200

    First desktop version of homepage

---
+++ main.js
@@ -0,0 +1,56 @@
+const referencesDots = Array.from(document.querySelectorAll('.btn--dot'));
+const referencesDotsInner = Array.from(document.querySelectorAll('.btn--dot>span'));
+
+let currentReferencesSlide = 0;
+
+const referencesCarousel = new Siema({
+    selector: '.references',
+    perPage: 2,
+    // draggable: false,
+    loop: true,
+    duration: 400
+});
+
+const referencesNext = () => {
+    referencesCarousel.next();
+
+    if(currentReferencesSlide === 2) {
+        currentReferencesSlide = 0;
+    }
+    else {
+        currentReferencesSlide++;
+    }
+
+    changeReferencesDotsStyle(currentReferencesSlide);
+}
+
+const referencesPrev = () => {
+    referencesCarousel.prev();
+
+    if(currentReferencesSlide === 0) {
+        currentReferencesSlide = 2;
+    }
+    else {
+        currentReferencesSlide--;
+    }
+
+    changeReferencesDotsStyle(currentReferencesSlide);
+}
+
+const referencesSlide = (n) => {
+    referencesCarousel.goTo(n);
+    currentReferencesSlide = n;
+
+    changeReferencesDotsStyle(n);
+}
+
+const changeReferencesDotsStyle = (n) => {
+    referencesDotsInner.forEach((item, index) => {
+        if(index === n) {
+            item.style.visibility = 'visible';
+        }
+        else {
+            item.style.visibility = 'hidden';
+        }
+    });
+}
